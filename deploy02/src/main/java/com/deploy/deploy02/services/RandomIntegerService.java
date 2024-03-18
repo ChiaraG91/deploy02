d110e6330ff8c68832cf7104ab48ece957a04787
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-18 12:04:09 +0100

    esercizio caricato

---
+++ deploy02/src/main/java/com/deploy/deploy02/services/RandomIntegerService.java
@@ -0,0 +1,16 @@
+package com.deploy.deploy02.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class RandomIntegerService {
+
+    public int getSum() {
+        Random random = new Random();
+        int num1 = random.nextInt(100);
+        int num2 = random.nextInt(100);
+        return num1 + num2;
+    }
+}
